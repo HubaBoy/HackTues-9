3e9a1c88ae6a7130c4c65efbd8e86b2d04d6a234
Nikieprogramach <101349864+Nikieprogramach@users.noreply.github.com>
2023-03-09 11:41:26 +0200

    Inventory can be opened and closed
    Inventory can be opened and closed

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+
+    public Transform itemParent;
+
+    Inventory inventory;
+
+    InventorySlot[] slots;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += UpdateUI;
+
+        slots = itemParent.GetComponentsInChildren<InventorySlot>();
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
