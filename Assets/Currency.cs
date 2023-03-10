298ecf23a31f69760e3599d90be90655511761ec
Nikieprogramach <101349864+Nikieprogramach@users.noreply.github.com>
2023-03-10 10:39:46 +0200

    Added money functionality
    Added crossscene money with the ability to be used

--- Assets/Currency.cs
+++ Assets/Currency.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Currency : MonoBehaviour
 {
     public static Currency instance;
 
-    private float money = 0;
+    public static float money = 0;
 
     public void AddMoney(float amount)
     {
---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MoneyManager : MonoBehaviour
+{
+    public Text moneyText;
+    public float moneyAmount;
+
+    public GameObject AmountOfMoney;
+    
+    void Start()
+    {
+        moneyText.text = "Money:" + moneyAmount;
+    }
+
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            AmountOfMoney.SetActive(false);
+        }
+        else
+        {
+            AmountOfMoney.SetActive(true);
+        }
+        moneyText.text = "Money:" + moneyAmount;
+    }
+
+    public void AddMoney(float amount)
+    {
+        moneyAmount += amount;
+    }
+
+    public void WithDraw(float amount)
+    {
+        moneyAmount -= amount;
+    }
+}
